ca8910d6ab72fe7661206e0d4aded3019466a9e9
Marco Castiello <marco.castiello@gmail.com>
2020-12-27 15:59:24 +0100

    Updated references

---
+++ pixi-reactive/precache-manifest.fc539caec2008a492474f4083a169dd5.js
@@ -0,0 +1,306 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47c70d0b1f3ca1ce6067ec67488a07a2",
+    "url": "./pixi-reactive/index.html"
+  },
+  {
+    "revision": "21b4743ce5b44fd9c118",
+    "url": "./pixi-reactive/static/css/65.173d43bd.chunk.css"
+  },
+  {
+    "revision": "becb09b3c48f2d2072cc",
+    "url": "./pixi-reactive/static/css/main.809d9e17.chunk.css"
+  },
+  {
+    "revision": "6167f52237bb658b719b",
+    "url": "./pixi-reactive/static/js/0.c6220479.chunk.js"
+  },
+  {
+    "revision": "0e68a261e4846a1e390826c53c553105",
+    "url": "./pixi-reactive/static/js/0.c6220479.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae6ad21fd8b7be05aa36",
+    "url": "./pixi-reactive/static/js/1.6612a376.chunk.js"
+  },
+  {
+    "revision": "94a6a0b1e6b9fe25c17672524a1b0c8c",
+    "url": "./pixi-reactive/static/js/1.6612a376.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6f9d933acc5236eb6a4",
+    "url": "./pixi-reactive/static/js/10.38e53bb2.chunk.js"
+  },
+  {
+    "revision": "3cd3ceb0bbfedeae61a1",
+    "url": "./pixi-reactive/static/js/11.5f3877c1.chunk.js"
+  },
+  {
+    "revision": "87e3af8f2122009b0eb1",
+    "url": "./pixi-reactive/static/js/12.634acffb.chunk.js"
+  },
+  {
+    "revision": "5a609ede6cc6170c97d7",
+    "url": "./pixi-reactive/static/js/13.66f82dd5.chunk.js"
+  },
+  {
+    "revision": "0f64a132e885b79b5d54",
+    "url": "./pixi-reactive/static/js/14.035911b3.chunk.js"
+  },
+  {
+    "revision": "375a3f020a6f00d45fd2",
+    "url": "./pixi-reactive/static/js/15.3cac9112.chunk.js"
+  },
+  {
+    "revision": "5ce147cda51a08b779a9",
+    "url": "./pixi-reactive/static/js/16.ad7e068a.chunk.js"
+  },
+  {
+    "revision": "ac5465d630dd9db47dc5",
+    "url": "./pixi-reactive/static/js/17.bfbff445.chunk.js"
+  },
+  {
+    "revision": "2dfe2e039b20f039f3a6",
+    "url": "./pixi-reactive/static/js/18.0726d9e1.chunk.js"
+  },
+  {
+    "revision": "bf69ea9cab9d5ee01483",
+    "url": "./pixi-reactive/static/js/19.8005c330.chunk.js"
+  },
+  {
+    "revision": "37da4ed351f1851cb33c",
+    "url": "./pixi-reactive/static/js/2.b26021e4.chunk.js"
+  },
+  {
+    "revision": "c4315b07696ee253ec02",
+    "url": "./pixi-reactive/static/js/20.e68ef118.chunk.js"
+  },
+  {
+    "revision": "fd5cb7446da23a14d79e",
+    "url": "./pixi-reactive/static/js/21.1aadc6b2.chunk.js"
+  },
+  {
+    "revision": "64235b6aed8a37443223",
+    "url": "./pixi-reactive/static/js/22.c40be272.chunk.js"
+  },
+  {
+    "revision": "8958c371bf825b69cc50",
+    "url": "./pixi-reactive/static/js/23.438111ef.chunk.js"
+  },
+  {
+    "revision": "5892528a89c08f77b378",
+    "url": "./pixi-reactive/static/js/24.04b37737.chunk.js"
+  },
+  {
+    "revision": "d30dc6713c7d2942cad9",
+    "url": "./pixi-reactive/static/js/25.2a044502.chunk.js"
+  },
+  {
+    "revision": "59b5888ccd4298792ce1",
+    "url": "./pixi-reactive/static/js/26.216bda76.chunk.js"
+  },
+  {
+    "revision": "323e65522c3af6ae86b5",
+    "url": "./pixi-reactive/static/js/27.d01a5381.chunk.js"
+  },
+  {
+    "revision": "287b6efd3a7adeef5c76",
+    "url": "./pixi-reactive/static/js/28.cf54e1e9.chunk.js"
+  },
+  {
+    "revision": "1e33f79fe84101628adc",
+    "url": "./pixi-reactive/static/js/29.b3fb0299.chunk.js"
+  },
+  {
+    "revision": "0ee78d963f3776733d06",
+    "url": "./pixi-reactive/static/js/3.727bf61f.chunk.js"
+  },
+  {
+    "revision": "3e0c0ef32b6bb7817906",
+    "url": "./pixi-reactive/static/js/30.38e94489.chunk.js"
+  },
+  {
+    "revision": "655a8f5a946d758d99da",
+    "url": "./pixi-reactive/static/js/31.1f90df8d.chunk.js"
+  },
+  {
+    "revision": "7e878c43fc70e65d8123",
+    "url": "./pixi-reactive/static/js/32.97754289.chunk.js"
+  },
+  {
+    "revision": "f97e53e9b7e7f438a9e2",
+    "url": "./pixi-reactive/static/js/33.f367c69b.chunk.js"
+  },
+  {
+    "revision": "06604c50d5fe80610f1d",
+    "url": "./pixi-reactive/static/js/34.277896e7.chunk.js"
+  },
+  {
+    "revision": "bd64b093bfc190d8c590",
+    "url": "./pixi-reactive/static/js/35.dd055b4e.chunk.js"
+  },
+  {
+    "revision": "49e9617cd0abb4f03623",
+    "url": "./pixi-reactive/static/js/36.218575a1.chunk.js"
+  },
+  {
+    "revision": "1e9e7dd89b5c1a477607",
+    "url": "./pixi-reactive/static/js/37.edfbeec4.chunk.js"
+  },
+  {
+    "revision": "30cb1e4797b5b5004e37",
+    "url": "./pixi-reactive/static/js/38.c0c7d330.chunk.js"
+  },
+  {
+    "revision": "9a43b17c4e81d4018b5b",
+    "url": "./pixi-reactive/static/js/39.353902de.chunk.js"
+  },
+  {
+    "revision": "1334db56368182d1e824",
+    "url": "./pixi-reactive/static/js/4.6a707928.chunk.js"
+  },
+  {
+    "revision": "7c4bb87dc3bae90d3f3c",
+    "url": "./pixi-reactive/static/js/40.7199c3e5.chunk.js"
+  },
+  {
+    "revision": "29945a8f45d7a51bd1d3",
+    "url": "./pixi-reactive/static/js/41.ec37b23f.chunk.js"
+  },
+  {
+    "revision": "3281d0d6aa85f043f5fb",
+    "url": "./pixi-reactive/static/js/42.d1217ffc.chunk.js"
+  },
+  {
+    "revision": "0d9744b8ecefd9c59bb4",
+    "url": "./pixi-reactive/static/js/43.4d9c8ab9.chunk.js"
+  },
+  {
+    "revision": "7b062d6420553ac22069",
+    "url": "./pixi-reactive/static/js/44.4d6d09f7.chunk.js"
+  },
+  {
+    "revision": "739da63099714ffa82f3",
+    "url": "./pixi-reactive/static/js/45.ad324404.chunk.js"
+  },
+  {
+    "revision": "411a58c6d5e3b05c17a3",
+    "url": "./pixi-reactive/static/js/46.73d7fa25.chunk.js"
+  },
+  {
+    "revision": "2be10388aa02e499930b",
+    "url": "./pixi-reactive/static/js/47.0930c405.chunk.js"
+  },
+  {
+    "revision": "b04bbc8b0e75bf9dbb5a",
+    "url": "./pixi-reactive/static/js/48.a72551fa.chunk.js"
+  },
+  {
+    "revision": "0e9bd80a2c20d3bb7e93",
+    "url": "./pixi-reactive/static/js/49.913be270.chunk.js"
+  },
+  {
+    "revision": "4471606e36e6273a9ad9",
+    "url": "./pixi-reactive/static/js/5.baf22379.chunk.js"
+  },
+  {
+    "revision": "b3f20197721564aebd24",
+    "url": "./pixi-reactive/static/js/50.07cab74e.chunk.js"
+  },
+  {
+    "revision": "dcacc04caa13a5f6e22d",
+    "url": "./pixi-reactive/static/js/51.74666e4b.chunk.js"
+  },
+  {
+    "revision": "d9a75036210fa7df2f12",
+    "url": "./pixi-reactive/static/js/52.74986437.chunk.js"
+  },
+  {
+    "revision": "81a2c8aef4d273a75b93",
+    "url": "./pixi-reactive/static/js/53.effc8fce.chunk.js"
+  },
+  {
+    "revision": "3273838789806e1f57c4",
+    "url": "./pixi-reactive/static/js/54.35d524d0.chunk.js"
+  },
+  {
+    "revision": "8c8a6eb4c775ca0c13ef",
+    "url": "./pixi-reactive/static/js/55.5430cc90.chunk.js"
+  },
+  {
+    "revision": "164813907bc3539bb819",
+    "url": "./pixi-reactive/static/js/56.846cfd70.chunk.js"
+  },
+  {
+    "revision": "26cf0f7fd75d41114f1b",
+    "url": "./pixi-reactive/static/js/57.72858e33.chunk.js"
+  },
+  {
+    "revision": "af30e4593f1efecd0c94",
+    "url": "./pixi-reactive/static/js/58.31cc8af5.chunk.js"
+  },
+  {
+    "revision": "57fc88e49b853ba7b7b4",
+    "url": "./pixi-reactive/static/js/59.d000d476.chunk.js"
+  },
+  {
+    "revision": "97e5c61dba6535152846",
+    "url": "./pixi-reactive/static/js/6.5003be71.chunk.js"
+  },
+  {
+    "revision": "5f6ada6b2acc132ae36b",
+    "url": "./pixi-reactive/static/js/60.2c6c435d.chunk.js"
+  },
+  {
+    "revision": "803118665014a9e24d1b",
+    "url": "./pixi-reactive/static/js/61.0fbee91b.chunk.js"
+  },
+  {
+    "revision": "8c6977afb4a2e3a9e510",
+    "url": "./pixi-reactive/static/js/62.682ea1d5.chunk.js"
+  },
+  {
+    "revision": "21b4743ce5b44fd9c118",
+    "url": "./pixi-reactive/static/js/65.d3568263.chunk.js"
+  },
+  {
+    "revision": "f9689ab3bdd81059c30c22857445580d",
+    "url": "./pixi-reactive/static/js/65.d3568263.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8109487425599fd30a8",
+    "url": "./pixi-reactive/static/js/7.37739169.chunk.js"
+  },
+  {
+    "revision": "4d4ed70441f24b4b5029",
+    "url": "./pixi-reactive/static/js/8.a0173164.chunk.js"
+  },
+  {
+    "revision": "cd7636800002f6cb72ce",
+    "url": "./pixi-reactive/static/js/9.66b486c5.chunk.js"
+  },
+  {
+    "revision": "becb09b3c48f2d2072cc",
+    "url": "./pixi-reactive/static/js/main.c388fbd4.chunk.js"
+  },
+  {
+    "revision": "14c1dac59dff80869fc8",
+    "url": "./pixi-reactive/static/js/runtime-main.d42bd0f9.js"
+  },
+  {
+    "revision": "1b6b2c3ed476f4d4b7555af75e387d73",
+    "url": "./pixi-reactive/static/media/Framework7Icons-Regular.1b6b2c3e.ttf"
+  },
+  {
+    "revision": "8f897db6f41a6ae1661072172143a21b",
+    "url": "./pixi-reactive/static/media/Framework7Icons-Regular.8f897db6.woff"
+  },
+  {
+    "revision": "9393ad14858229d680936a6206688704",
+    "url": "./pixi-reactive/static/media/Framework7Icons-Regular.9393ad14.woff2"
+  },
+  {
+    "revision": "f30e029218958bd6aad3cd424f865f94",
+    "url": "./pixi-reactive/static/media/Framework7Icons-Regular.f30e0292.eot"
+  }
+]);
--- pixi-reactive/service-worker.js
+++ pixi-reactive/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.b6e5485d290b849133d44df241e0057f.js"
+  "./pixi-reactive/precache-manifest.fc539caec2008a492474f4083a169dd5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./pixi-reactive/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
