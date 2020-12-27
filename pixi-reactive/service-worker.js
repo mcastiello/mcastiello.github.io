ef75b93ba611f0bcc54e4c4ba7aea92e0f60b590
Marco Castiello <marco.castiello@gmail.com>
2020-12-27 16:05:56 +0100

    Test path

--- pixi-reactive/service-worker.js
+++ pixi-reactive/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./pixi-reactive/precache-manifest.7e08382aa6d9929866c7106d06d016d5.js"
+  "./precache-manifest.af6db59655cd9c00d135d68b28e47dae.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./pixi-reactive/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
